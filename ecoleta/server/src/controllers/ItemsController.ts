255f247e4b69643a5ad02bc4b402f308767f055e
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-07-04 15:58:42 -0300

    Add mobile folder

--- ecoleta/server/src/controllers/ItemsController.ts
+++ ecoleta/server/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.titulo,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                image_url: `http://192.168.1.115:3333/uploads/${item.image}`,
             };
         });
     
